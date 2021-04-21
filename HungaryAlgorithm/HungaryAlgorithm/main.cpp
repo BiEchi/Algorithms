312c297457168bd718ca9416c3a7b54a35a792ff
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-04-21 11:19:54 +0800

    updated Hungarian

--- HungaryAlgorithm/HungaryAlgorithm/main.cpp
+++ HungaryAlgorithm/HungaryAlgorithm/main.cpp
@@ -8,20 +8,23 @@
 #include <iostream>
 using namespace std;
 
+// The Hungary Algorithm is used to find the perfect match in a bipartite graph in p-time.
+
 #define numOfElementsInLeftSet 4
 #define numOfElementsInRightSet 4
 
 int Map[numOfElementsInLeftSet][numOfElementsInRightSet]
-    = {1, 1, 0, 0,/**/ 1, 0, 1, 0,/**/ 0, 1, 0, 1,/**/ 0, 0, 0, 1};
+    = {1, 1, 1, 1,/**/ 0, 1, 0, 0,/**/ 1, 0, 0, 0,/**/ 0, 0, 0, 0};
 int p[numOfElementsInRightSet];
 bool visit[numOfElementsInRightSet];
 
 bool match(int i)
 {
-    for (int j = 1; j <= numOfElementsInLeftSet; j++) {
+    for (int j = 0; j < numOfElementsInLeftSet; j++) {
         if (Map[i][j] != 0 && visit[j] == 0) {
             visit[j] = true;
-            if (p[j] == 0 || match(p[j])) {
+            if (p[j] == 0 || match(p[j])) { // match(p[j]) means the initially matched left element can find a new match (not this one)
+                // RECURSION YES!!!
                 p[j] = i;
                 return true;
             }
@@ -33,13 +36,13 @@ bool match(int i)
 int Hungarian()
 {
     int count = 0;
-    for (int i = 1; i <= numOfElementsInLeftSet; i++) {
+    for (int i = 0; i < numOfElementsInLeftSet; i++) {
         memset(visit, 0, sizeof(visit));
         if (match(i)) {
             count++;
         }
     }
-    return count;
+    return count; // number of successfull matches
 }
 
 int main(int argc, const char * argv[]) {
